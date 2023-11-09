bcd837d40ee1d7f8c02838d4b125bf9e39b45e5d
Kenan Casey <kenancasey@gmail.com>
2023-11-08 23:17:20 -0600

    initial commit

---
+++ KthToLast/LinkedListExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace KthToLast
+{
+	public static class LinkedListExtensions
+	{
+		public static T KthToLastSinglyLinked<T>(this LinkedList<T> linkedList, int k)
+		{
+			return default;
+		}
+
+        public static T KthToLastDoublyLinked<T>(this LinkedList<T> linkedList, int k)
+        {
+			if ( k > linkedList.Count || k < 0 )
+			{
+				throw new IndexOutOfRangeException();
+			}
+
+			LinkedListNode<T> currentNode = linkedList.Last;
+
+			for( int i=0; i < k ; i++ )
+			{
+				currentNode = currentNode.Previous;
+            }
+
+            return currentNode.Value;
+
+        }
+    }
+}
+
---
+++ Polynomial/Polynomial.cs
@@ -0,0 +1,81 @@
+﻿using System;
+namespace Polynomial
+{
+
+	// 4x^3 + 9x - 5
+	public class Polynomial
+	{
+		LinkedList<Term> terms;
+
+		public int NumberOfTerms => terms.Count;
+
+		// TODO
+		public int Degree
+		{
+			get
+			{
+				if (NumberOfTerms == 0)
+					return 0;
+
+				return terms.First.Value.Power;
+			}
+
+		}
+		public Polynomial()
+		{
+			terms = new LinkedList<Term>();
+		}
+
+		// TODO
+		public void AddTerm(double coeff, int power)
+		{
+
+		}
+
+        public override string ToString()
+        {
+			string result = "";
+
+			var currentNode = terms.First;
+
+			while( currentNode != null)
+			{
+				//do work
+				result += currentNode.Value.ToString() + "+";
+				
+				currentNode = currentNode.Next;
+			}
+
+			return result;
+        }
+
+
+        // TODO
+        public static Polynomial Add(Polynomial p1, Polynomial p2)
+		{
+			return null;
+		}
+
+		// TODO
+		public static Polynomial Subtract(Polynomial p1, Polynomial p2)
+		{
+			return null;
+		}
+
+		// TODO
+		public static Polynomial Negate(Polynomial p)
+		{
+            return null;
+        }
+
+		// TODO
+		public static Polynomial Multiply(Polynomial p1, Polynomial p2)
+		{
+			return null;
+		}
+
+
+
+    }
+}
+
